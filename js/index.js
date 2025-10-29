9614e56344b2bbf22d4469cfeea821f266801668
cfo27 <c.fox@bellevuecollege.edu>
2025-10-29 14:32:07 -0700

    Added Score Tracking

--- js/index.js
+++ js/index.js
@@ -2,16 +2,16 @@ const choices = ["Bear", "Ninja", "Hunter"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
 
 function playGame(playerChoice){
   const computerChoice = choices[Math.floor(Math.random() * 3)];
   let result= "";
-  
- 
-
   if(playerChoice === computerChoice){
     result = "Tie Game";
-    console.log("Tie Game");
   }
   
   else{
@@ -27,8 +27,20 @@ function playGame(playerChoice){
         break;
     }
   }
-  
   playerDisplay.textContent = `PLAYER: ${playerChoice}`;
   computerDisplay.textContent = `Computer: ${computerChoice}`;
   resultDisplay.textContent = result;
+
+  switch(result){
+    case "Player Wins!":
+      resultDisplay.classList.add("whiteText");
+      playerScore++;
+      playerScoreDisplay.textContent = playerScore;
+      break;
+    case "Computer Wins!":
+      resultDisplay.classList.add("redText");
+      computerScore++;
+      computerScoreDisplay.textContent = computerScore;
+      break;
+  }
 }
